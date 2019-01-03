30f15e9a4e4971abc3a8413f25c4c76f62022647
Omkar <omkar@activesphere.com>
2019-01-03 14:52:08 +0530

    Add helper methods for Celcius to Fahrenheit and vice versa

--- Max6675.kt
+++ Max6675.kt
@@ -1,3 +1,4 @@
+import android.util.Log
 import com.google.android.things.pio.PeripheralManager
 import com.google.android.things.pio.SpiDevice
 import java.io.IOException
@@ -5,15 +6,30 @@ import java.io.IOException
 class Max6675 constructor(deviceName: String) {
 
     private val device: SpiDevice = PeripheralManager.getInstance().openSpiDevice(deviceName)
+    private val TAG: String = "MAX6675"
+
+    init {
+        configureSpiDevice()
+    }
 
     @Throws(IOException::class)
     fun configureSpiDevice() {
-        this.device.apply {
+        device.apply {
             setMode(SpiDevice.MODE0)
-            setFrequency(4300000)
+            setFrequency(5000000)
             setBitsPerWord(8);
             setBitJustification(SpiDevice.BIT_JUSTIFICATION_MSB_FIRST)
         }
+    }
+
+    @Throws(IOException::class)
+    fun closeSpiDevice(): Boolean {
+        try {
+            device.close()
+        } catch (ex: IOException) {
+            return false
+        }
+        return true
     }
 
     @Throws(IOException::class)
@@ -34,16 +50,41 @@ class Max6675 constructor(deviceName: String) {
         var dataFromDevice: Int = read16Bits()
 
         // Check for error reading value.
-        val andResult = dataFromDevice and (0x4).toInt()
-        if (andResult > 0) {
+        val errorCheckAnd = dataFromDevice and (0x4).toInt()
+        if (errorCheckAnd > 0) {
             return Double.NaN
         }
 
         // We only need the 12 MSBs.
-        val data12MSBs = dataFromDevice shr 3
+        var data12MSBs = dataFromDevice shr 3
+
+        // We need to check if the signed bit is set.
+        val negativeAnd = dataFromDevice and (0x80000000).toInt()
+        if (negativeAnd > 0) {
+            data12MSBs -= 4096
+        }
 
         // Scale by 0.25 degrees C per bit and return value.
         return data12MSBs.toDouble() * 0.25
+    }
+
+    fun getMeanTempInCelcius(numReadings: Int = 1): Double {
+        var totalTemp = 0.0
+        var i = 1
+        while(i++ <= numReadings) {
+            totalTemp += readTempInCelcius()
+            Thread.sleep(200)
+        }
+        return totalTemp / numReadings
+    }
+
+    companion object {
+        fun celciusToFahrenheit(tempInCelcius: Double): Double {
+            return ( tempInCelcius * ( 9.0/5.0 ) ) + 32
+        }
+        fun fahrenheitToCelcius(tempInFahrenheit: Double): Double {
+            return ( tempInFahrenheit - 32 ) * ( 5.0/9.0 )
+        }
     }
 }
 
